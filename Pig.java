4c7bb3eb34c3433ebaea7627a9270d2ba53507aa
Behlah-0612 <90437004+Behlah-0612@users.noreply.github.com>
2021-11-26 22:23:55 +0300

    Update Pig.java
    Pig Farmer Part 2 changes to Pig.java

--- Pig.java
+++ Pig.java
@@ -2,45 +2,85 @@ import java.util.Date;
 import java.time.LocalDate;
 public class Pig {
 
-    private static int pigcount = 0;
+    private static int pigCount = 0;
+    private LocalDate date;
 
     public static int getCount(){
-        return pigcount;
+        return pigCount;
+    }
+
+    public static void setCount(int PigCount) {
+        Pig.pigCount = pigCount;
     }
 
     private String name;
-    private String colour;
-    private String sex;
+    private String color;
+    private char sex;
+    private String PigID;
     private boolean Female;
+    private boolean isFed;
     private Date DOB;
 
     public boolean isFemale(){
-        return sex.equals("Female");
+        return false;
     }
     public String getName(){
         return name;
     }
 
-    public String getColour(){
-        return colour;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getSex(){
+    public String getColor(){
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public char getSex(){
         return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
     }
 
     public Date getDOB(){
         return DOB;
     }
 
-    Pig(String Name_n, String Colour_c, String Sex_s) {
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getPigID() {
+        return PigID;
+    }
+
+    public void setPigID(String pigID) {
+        this.PigID = pigID;
+    }
+    public boolean isFed() {
+        return isFed;
+    }
+
+    public void setFed(boolean isFed) {
+        this.isFed = isFed;
+    }
+
+
+    Pig(String PigID_p,String Name_n, String Colour_c, char Sex_s) {
+        PigID =PigID_p;
         name = Name_n;
         sex = Sex_s;
-        colour = Colour_c;
+        color = Colour_c;
         LocalDate DOB  =LocalDate.now();
 
     }
     public void feed(Feed nofood){
-        nofood.Consume();
+        nofood.consume();
     }
 }
